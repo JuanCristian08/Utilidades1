8911cd7a864ec062f8cb2d21c9650b47616b87bf
juanCristian08 <juanborba033@gmail.com>
2025-07-17 09:18:59 -0300

    commit

--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -9,19 +8,25 @@ class ApiService {
    */
   final _baseUrl = "http://10.0.2.2:3000";
 
-  Future<String?> login(Map<String, dynamic> data) async{
+  Future<String?> login(Map<String, dynamic> data) async {
     final url = Uri.parse('$_baseUrl/login');
+    
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(data),
+      ).timeout(Duration(seconds: 30)); // Timeout opcional (remova se quiser sem limite)
 
-    final response = await http.post(
-      url,
-      headers: {'Content-Type' : 'application/json'},
-      body: jsonEncode(data)
-    );
-
-    if(response.statusCode == 200){
-      final json = jsonDecode(response.body);
-      return json['token'];
-    }else{
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        return json['token'];
+      } else {
+        print('Erro no login: ${response.statusCode} - ${response.body}');
+        return null;
+      }
+    } catch (e) {
+      print('Exceção durante login: $e');
       return null;
     }
   }
