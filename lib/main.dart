521a887ece15a2233be0c8d94460ad4c8a467504
alexandre <alexandrepinto199014@hotmail.com>
2025-07-21 08:30:12 -0300

    criando classe riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -5,4 +5,15 @@ void main() {
   runApp(AppWidget());
 }
 
+/* 
+riverpod - é uma biblioteca de gereniamento de estado desenvolvida por Remi Rousselete (criador do provider).
+eke foi criado para corrigir limitações do provider e oferecer uma abordagem mais robusta, segura e flexível.
+
+vantagens
+- segura em tempo de compilação: detecta erros mais cedo
+- não depende de buildcontext
+- testável e modular
+- suporte nativo para providers assincronos, como futureProvider e streamProvider
+ */
+
 
