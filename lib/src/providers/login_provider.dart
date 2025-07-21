59f2c360f4c5be9f36490162019241c7f13b6b5b
alexandre <alexandrepinto199014@hotmail.com>
2025-07-21 09:38:18 -0300

    login river da view

---
+++ lib/src/providers/login_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:utilidades/src/controllers/login_controller.dart';
+import 'package:utilidades/src/controllers/loginriver_controller.dart';
+import 'package:utilidades/src/models/login_state.dart';
+
+final loginProvider = StateNotifierProvider<LoginriverController, LoginState>((ref) => LoginriverController());
